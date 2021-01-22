bd9ba636e7cfeb7eada89853b9b2c1d9ac8cd92e
Samuel Szabo <samuelszabo7@gmail.com>
2021-01-22 22:01:02 +0100

    phpunit?

--- config/app.php
+++ config/app.php
@@ -294,7 +294,7 @@ return [
             'driver' => Mysql::class,
             'persistent' => false,
             'timezone' => 'UTC',
-
+            'port' => getenv('MYSQL_PORT'),
             /*
              * For MariaDB/MySQL the internal default changed from utf8 to utf8mb4, aka full utf-8 support, in CakePHP 3.6
              */
@@ -337,6 +337,7 @@ return [
             'driver' => Mysql::class,
             'persistent' => false,
             'timezone' => 'UTC',
+            'port' => getenv('MYSQL_PORT'),
             //'encoding' => 'utf8mb4',
             'flags' => [],
             'cacheMetadata' => true,
